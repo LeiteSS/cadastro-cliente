0aeccfbd30d87bf59f8ab95b5539bb14b8225376
Leite, S. S <60820935+LeiteSS@users.noreply.github.com>
2022-09-14 08:40:36 -0300

    Add first rota

--- server.js
+++ server.js
@@ -8,6 +8,14 @@ const express = require("express");
 
 const app = express();
 
+/*
+  * ROTA GET: para retornar dados
+*/
+app.get("/", function(req, resp) {
+  resp.send("Hellow wolrd!");
+})
+
+// esta sempre deve ser a ultima linha quando usamos o express
 app.listen(8081, function() {
   console.log("Servidor funcionando na url http://loclahost:8081");
 })
