cc896aeb05fa49dde48eb35e5a1e56867958021b
LeiteSS <estoudente.program@gmail.com>
2022-09-14 09:03:37 -0300

    Fix and ignore node_modules

--- server.js
+++ server.js
@@ -17,5 +17,5 @@ app.get("/", function(req, resp) {
 
 // esta sempre deve ser a ultima linha quando usamos o express
 app.listen(8081, function() {
-  console.log("Servidor funcionando na url http://loclahost:8081");
+  console.log("Servidor funcionando na url http://localhost:8081");
 })
