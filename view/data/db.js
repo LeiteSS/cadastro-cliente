7e1744ca19aa7a082c16d6c39c2de2f53912bcb2
Leite, S. S <60820935+LeiteSS@users.noreply.github.com>
2022-09-28 10:16:29 -0300

    Add Connect function

--- view/data/db.js
+++ view/data/db.js
@@ -1,1 +1,10 @@
 // db.js
+async function connect() {
+  if(global.connection && global.connection.state !== 'disconnected') return global.connection;
+
+  const mysql = require("mysql2/promise");
+  const connection = await mysql.createConnection("mysql://root:root@loclhost:3306/webii");
+  global.connection = connection;
+
+  return connection;
+}
